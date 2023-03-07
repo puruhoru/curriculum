724040e4597041f8cb7fa1afab02d0b2996d8717
puruhoru <soul.man.take@gmail.com>
2023-03-07 19:58:20 +0900

    FizzBuzz問題

---
+++ 3-1/index.php
@@ -0,0 +1,34 @@
+<?php
+$x =3;
+$y =5;
+
+$i = 0;
+
+while($i < 101){
+   if($i == $x && $i == $y){
+    echo "<br>";
+    echo "FizzBuzz!!";
+
+    $i ++ ;
+    $x += 3;
+    $y += 5;
+   }elseif($i == $x ){
+        echo "<br>";
+        echo "Fizz!";
+        $x += 3;
+        $i ++;
+    }elseif($i == $y){
+        echo "<br>";
+        echo "Buzz!";
+        $y += 5;
+        $i ++;
+    }else{
+    echo"<br>";
+    echo $i;
+    $i ++;  
+    }
+
+}
+
+
+?>
